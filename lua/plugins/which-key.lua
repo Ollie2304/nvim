aa11a2556d4b5ccdc0957153b77ad9c8ce1b5278
Ollie <github@ollie2304.co.uk>
2024-07-13 04:24:51 +0100

    update which-key mappings to v3 spec

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,24 +3,20 @@ return {
 		"folke/which-key.nvim",
 		event = "VimEnter",
 		config = function()
-			require("which-key").setup()
-
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ebug", _ = "which_key_ignore" },
-				["<leader>dt"] = { name = "[T]rouble", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>st"] = { name = "[S]earch [T]odo", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>gh"] = { name = "[G]it [H]unk", _ = "which_key_ignore" },
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>c", desc = "[C]ode" },
+				{ "<leader>d", desc = "[D]ebug" },
+				{ "<leader>dt", desc = "[T]rouble" },
+				{ "<leader>r", desc = "[R]ename" },
+				{ "<leader>s", desc = "[S]earch" },
+				{ "<leader>st", desc = "[S]earch [T]odo" },
+				{ "<leader>w", desc = "[W]orkspace" },
+				{ "<leader>t", desc = "[T]oggle" },
+				{ "<leader>gh", desc = "[G]it [H]unk" },
+				{ "<leader>g", desc = "[G]it" },
+				{ "<leader>h", desc = "[H]arpoon" },
 			})
-			require("which-key").register({
-				["<leader>gh"] = { "[G]it [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 }
